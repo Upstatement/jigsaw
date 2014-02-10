c52ff04e60ad492e055bfc8ee33ca1a65d8cb101
Jared Novack <jared@upstatement.com>
2014-02-10 14:34:26 -0500

    Update jigsaw.php

--- jigsaw.php
+++ jigsaw.php
@@ -4,7 +4,7 @@
 	Plugin Name: Jigsaw
 	Description: Simple ways to make admin customizations for WordPress
 	Author: Jared Novack + Upstatement
-	Version: 0.4.8
+	Version: 0.4.9
 	Author URI: http://jigsaw.upstatement.com/
 	*/
 
